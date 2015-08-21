141bdfd05591b15a135c31d1abfd9ad169ba5081
Jackie Yang <Jackie@MacBook-Pro.local>
2015-08-21 02:10:17 -0400

    update with beacon app id

--- common.sh
+++ common.sh
@@ -7,12 +7,12 @@ export DEV_1=d74e6846
 export DEV_2=928c8b15
 
 # Application String
-export BLUEWAVE_BEACON_APP=com.example.jackie.bluewaveserver
-export BLUEWAVE_BEACON_ACT=com.example.jackie.bluewaveserver.MainActivity
-export BLUEWAVE_SCANNING_APP=com.example.jackie.bluewaveserver
-export BLUEWAVE_SCANNING_ACT=com.example.jackie.bluewaveserver.MainActivity
-export BLUETOOTHWAVE_APP=com.example.jackie.bluewaveserver
-export BLUETOOTHWAVE_ACT=com.example.jackie.bluewaveserver.MainActivity
+export BLUEWAVE_BEACON_APP=com.adefreitas.magicappserver
+export BLUEWAVE_BEACON_ACT=com.adefreitas.magicappserver.MainActivity
+export BLUEWAVE_SCANNING_APP=com.adefreitas.magicappserver
+export BLUEWAVE_SCANNING_ACT=com.adefreitas.magicappserver.MainActivity
+export BLUETOOTHWAVE_APP=com.example.jackie.bluewaveclient
+export BLUETOOTHWAVE_ACT=com.example.jackie.bluewaveclient.MainActivity
 
 # Common Directory
 export BLUEWAVE_DIR=/home/sshuser/bluewave
--- post.sh
+++ post.sh
@@ -6,8 +6,8 @@
 source /root/bluewave/common.sh
 
 # stop application
-# adb -s $DEV_1 shell am force-stop $BLUEWAVE_BEACON_APP
-# adb -s $DEV_2 shell am force-stop $BLUEWAVE_BEACON_APP
+adb -s $DEV_1 shell am force-stop $BLUEWAVE_BEACON_APP
+adb -s $DEV_2 shell am force-stop $BLUEWAVE_BEACON_APP
 
 # dump debug info
 adb -s $DEV_1 bugreport > $BLUEWAVE_DIR/$DATE-device-1.bugreport
